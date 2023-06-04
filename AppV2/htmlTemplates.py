96140b86bf16d4da106734030d0f3507e350a773
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-06-03 21:37:37 -0400

    Create htmlTemplates.py

---
+++ AppV2/htmlTemplates.py
@@ -0,0 +1,46 @@
+# CSS styling
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 85%;
+  padding: 0 1.25rem;
+  color: #fff;
+}
+'''
+
+# Define user and bot templates
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://resizing.flixster.com/ocuc8yjm8Fu5UK5Ze8lbdp58m9Y=/300x300/v2/https://flxt.tmsimg.com/assets/p11759522_i_h9_aa.jpg">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://www.shutterstock.com/image-vector/nerd-robot-vector-illustration-version-260nw-2126944997.jpg">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
